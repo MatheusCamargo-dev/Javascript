c92a0599202f1edeabd7b67429a570ea8f2ecb94
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2022-10-03 17:46:08 -0300

    Initial commit

---
+++ modelo/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modelo</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script src="./assets/js/main.js" defer></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Lorem ipsum dolor sit amet </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laborum est harum ipsa possimus provident a ducimus minima laboriosam tenetur nisi fuga perspiciatis, dignissimos repellendus amet accusamus error facilis. Non.</p>
+
+        <form action="">
+            <div class="form">
+                <div>
+                    <label for="input-test-1">Label teste #1</label>
+                    <input type="text" id="input-test-1">
+                </div>
+                <div>
+                    <label for="input-test-2">Label teste #2</label>
+                    <input type="password" id="input-test-2">
+                </div>
+                <div>
+                    <label for="input-test-3">Label teste #3</label>
+                    <input type="email" id="input-test-3">
+                </div>
+                <div>
+                    <label for="input-test-4">Label teste #4</label>
+                    <input type="number" id="input-test-4">
+                </div>
+                <button type="submit"> Salvar </button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
