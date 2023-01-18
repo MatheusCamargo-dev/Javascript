394d71561330659126bd94ca08c1fa79145c0e42
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-01-17 23:02:42 -0300

    FizzBuzz

---
+++ modelo/assets/js/FizzBuzz.js
@@ -0,0 +1,28 @@
+// divisivel por 3 = Fizz
+// divisivel por 5 = Buzz
+// divisivel por 5 e 3 = FizzBuzz
+// não divisivel por 5 e 3 = Retorna o proprio numero
+const random = (min, max) => {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r)
+}
+
+const FizzBuzz = (num) => {
+    let retorno
+    if(num % 3 == 0){
+        retorno =  'Fizz'
+    }else if(num % 5 == 0){
+        retorno = 'Buzz'
+    }else if(!(num % 5 == 0) && !(num % 3 == 0)){
+        retorno = num
+    }else if((num % 5 == 0) && (num % 3 == 0)){
+        retorno = 'FizzBuzz';
+    }
+
+    return {dado: retorno, numero: num}
+}
+
+const {dado: resultado, numero} = FizzBuzz(random(1,10))
+console.log(resultado, numero)
+
+
