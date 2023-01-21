0e22086db52279d4cebcd278b5eaec844dd7aef4
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-01-20 23:57:06 -0300

    lista tarefas v3

--- modelo/assets/js/lista_tarefas.js
+++ modelo/assets/js/lista_tarefas.js
@@ -10,51 +10,101 @@ const criaLi = (tarefa) => {
     return li
 }
 
-const criaSpan = () => {
+const criaSpan = (tarefa) => {
     let span = document.createElement('span')
+    span.innerHTML = tarefa
     return span
 }
 
 const criaBtnApagar = (tarefa) => {
     let btnApagar = document.createElement('input')
-    btnApagar.tarefa = tarefa
     btnApagar.value = 'Apagar'
     btnApagar.type = 'button'
     btnApagar.className = 'excluiTarefa ml-10'
-    btnApagar.setAttributeNode = 'tarefa'
+    btnApagar.tarefa = tarefa
     btnApagar.addEventListener('click', function(){
         apagarTarefa(this);
     })
     return btnApagar
 }
 
-const adicionaTarefa = (tarefa) => {
-    listaTarefas.appendChild(tarefa)
+const validaTarefa = () => {
+    if(tarefa.value != ''){
+        return true
+    }else{
+        return window.alert('informe uma tarefa!')
+    }
+}
+
+const getTarefaValue = () => {
+    let tarefaValue = tarefa.value
+    return tarefaValue
+}
+
+const adicionaTarefa = (li, btnApagar, span) => {
+    li.appendChild(span)
+    li.appendChild(btnApagar)
+    listaTarefas.appendChild(li)
 }
 
 const criaTarefa = (tarefa) => {
     let li = criaLi(tarefa)
     let btnApagar = criaBtnApagar(tarefa)
-    let span = criaSpan()
-    span.innerHTML = tarefa
-
-    li.appendChild(span)
-    li.appendChild(btnApagar)
-    adicionaTarefa(li)
+    let span = criaSpan(tarefa)    
+    adicionaTarefa(li, btnApagar, span)
+    atualizaTarefaJSON()
+    limpaInputTarefa()
 }
 
+const atualizaTarefaJSON = () => {
+    let arrayTarefas = listaTarefas.querySelectorAll('li')
+    let listaDeTarefas = []
+    for(let tarefa of arrayTarefas){
+        listaDeTarefas.push(tarefa.innerText.trim())
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas)
+    localStorage.setItem('tarefas', tarefasJSON)
+}
+
+const getTarefasLocalStorage = () => {
+    let tarefas = localStorage.getItem('tarefas');
+    let listaDeTarefas = tarefas = JSON.parse(tarefas);
+    return listaDeTarefas
+}
+
+const listaTarefasLocalStorage = () => {
+    let listaDeTarefas = getTarefasLocalStorage()
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa)
+    }
+
+}
 const apagarTarefa = (tarefa) => {
+    console.log(tarefa.tarefa)
     document.getElementById(tarefa.tarefa).remove()
+    atualizaTarefaJSON()
+}
+
+const limpaInputTarefa = () => {
+    tarefa.value = ''
+    tarefa.focus()
 }
 
 addTarefa.addEventListener('click', function(){
-    if(tarefa.value != ''){
-        let textoTarefa = tarefa.value
-        criaTarefa(textoTarefa)
-    }else{
-        window.alert('informe uma tarefa!')
+    if(validaTarefa()){
+        let tarefaValue = getTarefaValue()
+        criaTarefa(tarefaValue)
     }
 })
 
+tarefa.addEventListener('keypress', function(e){
+    if(e.keyCode === 13){
+        if(validaTarefa()){
+            let tarefaValue = getTarefaValue()
+            criaTarefa(tarefaValue)
+        }
+    }
+})
 
+listaTarefasLocalStorage()
 
