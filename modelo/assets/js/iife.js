bad04332fabbc46b5208e7740e612d16b1876490
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-01-25 13:57:15 -0300

    IIFE

---
+++ modelo/assets/js/iife.js
@@ -0,0 +1,9 @@
+// IIFE -> Imediataly invoked function expression
+(function(nome){
+   const idade = 16
+   console.log(nome)
+   console.log(idade)
+})('nick');
+
+const idade = 18;
+console.log(idade)
