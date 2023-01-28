84456cc830ab43dc422a46b2cd1df5e364e1ae68
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-01-28 10:26:48 -0300

    att model

--- modelo/assets/js/calculadora.js
+++ modelo/assets/js/calculadora.js
@@ -1,32 +1,21 @@
-
-const rand = (min = 1000, max = 3000) => {
-    const r = Math.random() * (max-min) + min;
-    return Math.floor(r)
+const calculadora = {
+    operador,
+    calcula: function(operador, valor1, valor2){
+        let resultado;
+        if (operador === '+') resultado = (valor1 + valor2);
+        if (operador === '-') resultado = (valor1 - valor2);
+        if (operador === '*') resultado = (valor1 * valor2);
+        if (operador === '/') resultado = (valor1 / valor2);
+        return resultado
+    }
 }
 
-function f1(callback){
-    setTimeout(function(){
-        console.log('f1')
-        if (callback) callback();
-    }, rand())
+const operadores = document.querySelectorAll('.operador');
+var operador;
+console.log(operadores)
+for(let op of operadores){
+   op.addEventListener('click', function(){
+      calculadora.operador = this.innerHTML
+      console.log(calculadora.operador)
+   })
 }
-function f2(callback){
-    setTimeout(function(){
-        console.log('f2')
-        if (callback) callback();
-    }, rand())
-}
-function f3(callback){
-    setTimeout(function(){
-        console.log('f3')
-        if (callback) callback();
-    }, rand())
-}
-
-f1(function(){
-    f2(function(){
-        f3(function(){
-            console.log('teste')
-        })
-    })
-})
