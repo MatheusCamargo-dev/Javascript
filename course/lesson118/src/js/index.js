81484f7fe2f4930c794e4365cd3f93652543dada
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-02-21 21:16:33 -0300

    Import/Export in js (lesson 118)

--- course/lesson118/src/js/index.js
+++ course/lesson118/src/js/index.js
@@ -1,4 +1,14 @@
-// import * as module1 from './module1';
+// import defaultExport from "module-name";
+// import * as name from "module-name";
+// import { export } from "module-name";
+// import { export as alias } from "module-name";
+// import { export1 , export2 } from "module-name";
+// import { foo , bar } from "module-name/path/to/specific/un-exported/file";
+// import { export1 , export2 as alias2 , [...] } from "module-name";
+// import defaultExport, { export [ , [...] ] } from "module-name";
+// import defaultExport, * as name from "module-name";
+// import "module-name"; var promise = import("module-name"); // This is a stage 3 proposal.
+
 import Person, {name as nameModule, last_name, age, vanilla} from './module1';
 const print = (...args) => console.log(...args);
 
