73b158e4fdf7246f1b57c81527d57b4c8fa04469
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-02-25 18:04:16 -0300

    nodemon and new node template (lesson7)

---
+++ node/lesson7/server.js
@@ -0,0 +1,29 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//          POST    GET   PUT    DELETE
+
+app.get('/', (request, response) => {
+    response.send(`
+        <form action="/" method="POST">
+        Name: <input type="text" name="name">
+        <button>Submit</button>
+        </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('/contact', (request, response) => {
+    response.send('Contact us!');
+});
+
+app.listen(3333, () => {
+    console.log('Server running in port 3333');
+    console.log('webserver: http://localhost:3333');
+
+});
