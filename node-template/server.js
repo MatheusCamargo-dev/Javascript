8128454f3989c21e8fdf5afcef4288ee2098a441
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-02-25 19:22:57 -0300

    update template with routes (lesson 9)

--- node-template/server.js
+++ node-template/server.js
@@ -1,17 +1,8 @@
 const express = require('express');
+const routes = require('./routes');
+
 const app = express();
-app.listen(3333, () => console.log('webserver: http://localhost:3333'));
 app.use(express.urlencoded({extended: true}))
+app.use(routes);
 
-app.get('/', (request, response) => {
-    response.send(`
-        <form action="/" method="POST">
-        Name: <input type="text" name="name">
-        <button>Submit</button>
-        </form>
-    `);
-});
-
-app.post('/', (req, res) => {
-    res.send('')
-})
+app.listen(3333, () => console.log('webserver: http://localhost:3333'));
