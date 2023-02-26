0ca162ee144a1a4803102c906f8937cc1603b5f0
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-02-26 03:43:29 -0300

    update template

--- node-template/server.js
+++ node-template/server.js
@@ -1,14 +1,26 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
 
-app.use(express.urlencoded({extended: true}))
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DBPASSWORD)
+        .then( () => {
+            app.emit('connected')
+        });
 
+
+app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.resolve(__dirname, 'public')))
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(routes);
-
-app.listen(3333, () => console.log('webserver: http://localhost:3333'));
+    
+    
+app.on('connected', () => {
+    console.log('connected to data base with success!')
+    app.listen(3333, () => console.log('webserver: http://localhost:3333'));
+})
