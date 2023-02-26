bd263d8c1894519dc6e9962f3140719ecb09e5a1
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-02-26 16:49:07 -0300

    using csurf and helmet (lesson17)

---
+++ node/lesson17/src/controllers/homeController.js
@@ -0,0 +1,10 @@
+exports.get = (req, res) =>{
+    res.render('index', {
+        title: 'this is my <span class="text-info">title</span>',
+        numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    })
+}
+
+exports.post = (req, res) => {
+    res.send(req.body)
+}
