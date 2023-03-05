9f460e12d7e41494b0d7fddf49ac63085c2e96f9
matheuscamargo <matheuscamargodev@gmail.com>
2023-03-05 09:26:44 -0300

    CRUD complete for Students

--- API_REST/src/controllers/StudentController.js
+++ API_REST/src/controllers/StudentController.js
@@ -1,9 +1,97 @@
 import Student from '../models/Student';
 
 class StudentController {
+  async store(req, res) {
+    try {
+      const newStudent = await Student.create(req.body);
+      return res.json({ newStudent });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
   async index(req, res) {
     const students = await Student.findAll({ attributes: ['id', 'name', 'last_name', 'email', 'age', 'weight', 'height'] });
     res.json(students);
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).res.json({
+          errors: ['Missing id.'],
+        });
+      }
+      const student = await Student.findByPk(id);
+      if (!student) {
+        return res.status(400).json({
+          errors: ['student not found.'],
+        });
+      }
+      const { name, email } = student;
+      return res.json({ id, name, email });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing id.'],
+        });
+      }
+
+      const student = await Student.findByPk(req.params.id);
+
+      if (!student) {
+        return res.status(400).json({
+          errors: ['student doesn\'t exits.'],
+        });
+      }
+
+      const update = await student.update(req.body);
+      const { id, name, email } = update;
+      return res.json({ id, name, email });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing id.'],
+        });
+      }
+
+      const student = await Student.findByPk(req.params.id);
+
+      if (!student) {
+        return res.status(400).json({
+          errors: ['student doesn\'t exits.'],
+        });
+      }
+
+      await student.destroy();
+      return res.json({
+        deleted: true,
+        msg: 'student deleted with success!',
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
   }
 }
 
--- API_REST/src/models/Student.js
+++ API_REST/src/models/Student.js
@@ -3,12 +3,65 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      last_name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      age: Sequelize.INTEGER,
-      weight: Sequelize.FLOAT,
-      height: Sequelize.FLOAT,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'The name must be between 3 and 255 characters',
+          },
+        },
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'The last name must be between 3 and 255 characters',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email already registered.',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Invalid email.',
+          },
+        },
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Age needs to be an integer.',
+          },
+        },
+      },
+      weight: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Weight must be a valid number.',
+          },
+        },
+      },
+      height: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Height must be a valid number.',
+          },
+        },
+      },
     }, {
       sequelize,
     });
