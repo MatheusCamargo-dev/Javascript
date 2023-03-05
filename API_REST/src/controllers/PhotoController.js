3ed5d08c46a45b569012c0b4495c71c02fef0400
matheuscamargo <matheuscamargodev@gmail.com>
2023-03-05 11:07:49 -0300

    upload files (img) using multer

---
+++ API_REST/src/controllers/PhotoController.js
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('photo');
+
+class PhotoController {
+  async store(req, res) {
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(401).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
+  }
+}
+
+export default new PhotoController();
---
+++ API_REST/src/routes/photoRoutes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import photoController from '../controllers/PhotoController';
+
+const router = new Router();
+
+router.post('/', photoController.store);
+export default router;
